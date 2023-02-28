f3edd7d107b3209836ed219b482d3dd3d284563e
Ahmet Burhan Simsek <ahmet-simsek@hotmail.com>
2023-02-28 11:17:37 +0300

    Add project files.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "Do Not Lock is running. Enjoy your non-lockable computer.";
+            this.notifyIcon1.BalloonTipTitle = "Do Not Lock";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Do Not Lock";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(16, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(316, 57);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "\"Do_Not_Lock\" is not active";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(8, 23);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(177, 20);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Start on Windows Startup";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(208, 17);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Activate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 120000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 85);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Do_Not_Lock";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Timer timer1;
+	}
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using IWshRuntimeLibrary;
+using System.Runtime.InteropServices;
+using File = System.IO.File;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        //dirName = C:\Users\USERNAME\AppData\Roaming\Do_Not_Lock
+        public String dirName = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\Do_Not_Lock";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //importing user32.dll for sending key command to machine
+        //Key.Send function is not preventing machine to not locked
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, uint dwExtraInfo);
+        const uint KEYEVENTF_KEYUP = 0x2;
+
+         
+        //Creating shortcut to startup directory
+        private void CreateShortcut()
+        {
+             string link = Environment.GetFolderPath( Environment.SpecialFolder.Startup ) 
+                            + Path.DirectorySeparatorChar + Application.ProductName + ".lnk";
+            var shell = new WshShell();
+            var shortcut = shell.CreateShortcut( link ) as IWshShortcut;
+            shortcut.TargetPath = Application.ExecutablePath;
+            shortcut.WorkingDirectory = Application.StartupPath;
+            //shortcut...
+            shortcut.Save();
+        }
+
+        //check the startup setting from C:\Users\USERNAME\AppData\Roaming\Do_Not_Lock\settings.txt
+        private void checkStartup()
+        {
+            string[] startupCheck = System.IO.File.ReadAllLines(dirName + "\\settings.txt");
+            if(startupCheck[0] == "true")
+            {
+                checkBox1.Checked = true;
+            }
+            if(startupCheck[0] == "false")
+            {
+                checkBox1.Checked = false;
+            }
+        }
+
+        //Create folder for Do_Not_Lock save file
+        public void createFolder()
+        {
+            //Check that the directory exists or not
+            if (!Directory.Exists(dirName))
+            {
+                Directory.CreateDirectory(dirName); //if not, create it
+                using (System.IO.StreamWriter setting = new System.IO.StreamWriter(dirName + "\\settings.txt", true))
+                {
+                    //set startup setting to true default
+                    setting.WriteLine("true");
+                    setting.Close();
+                }
+            }
+                
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            createFolder();
+            CreateShortcut();
+            checkStartup();
+        }
+
+        //minimize the form to system tray
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                notifyIcon1.Visible = true;
+                notifyIcon1.ShowBalloonTip(500);
+                this.Hide();
+            }
+
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                notifyIcon1.Visible = false;
+            }
+        }
+
+        //double click on notification will show the form
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        bool check = false;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //if Do_Not_Lock is activated;
+            if(check == false)
+            {
+                button1.Text = "Activated";
+                groupBox1.Text = "\"Do_Not_Lock\" is active";
+                notifyIcon1.Text = "\"Do_Not_Lock\" is active";
+                timer1.Enabled = true;
+				timer1_Tick(sender, e);
+                check = true;
+            }
+            //if Do_Not_Lock is not activated;
+            else
+            {
+                button1.Text = "Activate";
+                groupBox1.Text = "\"Do_Not_Lock\" is not active";
+                notifyIcon1.Text = "\"Do_Not_Lock\" is not active";
+                timer1.Enabled = false;
+                check = false;
+            }
+        }
+        
+        //Run on startup setting checkbox
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+                //if checked, it will run on startup automatically
+                if (checkBox1.Checked == true)
+                {
+                    //this setting is saved in text file
+                    //if checkbox is checked, it will write "true" to text file
+                    System.IO.File.Delete(dirName + "\\settings.txt");
+                    using (System.IO.StreamWriter setting = new System.IO.StreamWriter(dirName + "\\settings.txt", true))
+                        setting.WriteLine("true");
+                }
+                else
+                {
+                    //if checkbox is not checked, it will write "false" to text file
+                    System.IO.File.Delete(dirName + "\\settings.txt");
+                    using (System.IO.StreamWriter setting = new System.IO.StreamWriter(dirName + "\\settings.txt", true))
+                        setting.WriteLine("false");
+                }
+
+        }
+
+        //timer tick function
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //send NumLock key to machine for twice,
+            //It is just sending key press twice so that you'll not feel anything
+            keybd_event((byte)Keys.NumLock, 0, 0, 0);
+            keybd_event((byte)Keys.NumLock, 0, KEYEVENTF_KEYUP, 0);
+            keybd_event((byte)Keys.NumLock, 0, 0, 0);
+            keybd_event((byte)Keys.NumLock, 0, KEYEVENTF_KEYUP, 0);
+        }
+    }
+}
